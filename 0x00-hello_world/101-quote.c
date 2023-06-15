7f2bf4fa1fcfb19f0a1752fd833c4b27f485f5da
Mongezi Mthimkulu <mongezimsl@gmail.com>
2023-06-15 11:29:17 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char str1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	putchar(str1);
+	char cd[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, cd, 59);
 	return (1);
 }
