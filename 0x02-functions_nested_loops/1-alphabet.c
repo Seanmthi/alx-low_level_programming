4ca8ddae7dcc048d4a4a89d97a1e621c4e3a44a2
Mongezi Mthimkulu <mongezimsl@gmail.com>
2023-06-20 07:44:59 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  */
 void print_alphabet(void)
 {
-        char  i = 97;
-	while(i<123)
+        int i = 97;
+	while(i < 123)
 	{
 		_putchar(i);
 		i++;
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
