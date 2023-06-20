0988bb525f0f3d3fd561783fb8cf74e5014174b8
Mongezi Mthimkulu <mongezimsl@gmail.com>
2023-06-20 10:27:43 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - determines the absolute value
+ *
+ * @n: integer
+ *
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
