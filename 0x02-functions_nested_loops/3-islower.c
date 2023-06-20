2f5bf55b25c5d554947804bddbc0624d1edd2b20
Mongezi Mthimkulu <mongezimsl@gmail.com>
2023-06-20 08:49:10 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - shows 1 if character is lower case and 0 otherwise
+ *
+ * Return: 1 if lower case  or 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	putchar('\n');
+}
