d2a29913eb058b2aae59ae2db8988df8c6b8708d
Mongezi Mthimkulu <mongezimsl@gmail.com>
2023-06-20 13:14:37 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,22 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
 		{
 			m = a * b;
-			_putchar(m + 48);
 			_putchar(44);
 			_putchar(32);
+			if (m <= 9)
+			{
+				_putchar(32);
+				_putchar(m + 48);
+			}
+			else
+			{
+				_putchar((m / 10) + 48);
+				_putchar((m % 10) + 48);
+			}
 		}
 		_putchar('\n');
 	}
