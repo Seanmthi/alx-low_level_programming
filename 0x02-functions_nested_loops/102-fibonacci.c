94d511dbcacff1cdfd9468d9b7f6316a01345bce
Mongezi Mthimkulu <mongezimsl@gmail.com>
2023-06-21 12:27:46 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long int a, b, c;
+
+	a = 1;
+	b = 2;
+	printf("%ld, %ld", a, b);
+	for (i = 0; i < 48; i++)
+	{
+		c = a + b;
+		printf(", %ld", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return 0;
+}
